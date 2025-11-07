cecbc004fbf44ee6fc5e52a7e892fecd9c833e42
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-11-07 11:03:42 -0800

    Add Timeline component to HomePage for enhanced content structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "./HeroSection";
+import Timeline from "./Timeline";
 
 export default function HomePage() {
   return (
     <>
       <HeroSection />
+      <Timeline />
       {/* ...other homepage sections... */}
     </>
   );
